75c7a9e087710654ca33f2c162bd1e61bfbc25b9
tikitko <bns.6587@gmail.com>
2023-06-13 00:48:15 +0300

    naming

--- blog-server-services/src/traits/user_service.rs
+++ blog-server-services/src/traits/user_service.rs
@@ -5,12 +5,17 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct User {
     pub username: String,
+    #[serde(rename = "firstName")]
     pub first_name: Option<String>,
+    #[serde(rename = "middleName")]
     pub middle_name: Option<String>,
+    #[serde(rename = "lastName")]
     pub last_name: Option<String>,
     pub mobile: Option<String>,
     pub email: Option<String>,
+    #[serde(rename = "passwordHash")]
     pub password_hash: String,
+    #[serde(rename = "registeredAt")]
     pub registered_at: DateTime,
     pub status: Option<String>,
 }
