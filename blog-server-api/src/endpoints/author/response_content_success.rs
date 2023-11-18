6b3b1314a9533091b652c5e55e1d18820bb10c2a
Mikita Bykau <bns.6587@gmail.com>
2023-11-18 20:30:05 +0100

    client-side-generic (#50)
    * client-side-generic
    
    * fix typo

--- blog-server-api/src/endpoints/author/response_content_success.rs
+++ blog-server-api/src/endpoints/author/response_content_success.rs
@@ -5,7 +5,7 @@ use screw_api::response::{ApiResponseContentBase, ApiResponseContentSuccess};
 
 #[derive(Debug, Clone)]
 pub struct AuthorResponseContentSuccess {
-    container: AuthorContainer,
+    pub container: AuthorContainer,
 }
 
 impl Into<AuthorResponseContentSuccess> for ServiceAuthor {
