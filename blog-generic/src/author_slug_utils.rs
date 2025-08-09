9f48627b3a5c75361f406eaae3f10a33b9d839c8
Mikita Bykau <bns.6587@gmail.com>
2025-08-09 09:09:41 +0200

    test: cover author endpoint

--- blog-generic/src/author_slug_utils.rs
+++ blog-generic/src/author_slug_utils.rs
@@ -9,3 +9,45 @@ pub fn clean(slug: &String) -> String {
 pub fn extend(slug: &String, suffix: &String) -> String {
     format!("{slug}{SPLIT_SYMBOL}{suffix}")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn clean_removes_suffix() {
+        let slug = String::from("author^extra");
+        assert_eq!(clean(&slug), "author");
+    }
+
+    #[test]
+    fn clean_returns_original_when_no_symbol() {
+        let slug = String::from("author");
+        assert_eq!(clean(&slug), "author");
+    }
+
+    #[test]
+    fn clean_handles_multiple_symbols() {
+        let slug = String::from("author^mid^end");
+        assert_eq!(clean(&slug), "author^mid");
+    }
+
+    #[test]
+    fn clean_handles_empty_string() {
+        let slug = String::new();
+        assert_eq!(clean(&slug), "");
+    }
+
+    #[test]
+    fn extend_joins_slug_and_suffix() {
+        let slug = String::from("author");
+        let suffix = String::from("meta");
+        assert_eq!(extend(&slug, &suffix), "author^meta");
+    }
+
+    #[test]
+    fn extend_allows_empty_parts() {
+        assert_eq!(extend(&String::new(), &String::from("meta")), "^meta");
+        assert_eq!(extend(&String::from("author"), &String::new()), "author^");
+    }
+}
