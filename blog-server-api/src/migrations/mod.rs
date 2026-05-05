5be5e2b1739a59abd48ed7e3cff30e7b8c441923
tikitko <bns.6587@gmail.com>
2026-05-05 16:55:37 +0200

    db placeholder for demo

--- blog-server-api/src/migrations/mod.rs
+++ blog-server-api/src/migrations/mod.rs
@@ -1,8 +1,10 @@
 mod base;
 mod content_formatting;
+mod seed_placeholder_ru;
 
 pub async fn exec(rb: &rbatis::RBatis) -> Result<(), Box<dyn std::error::Error>> {
     base::exec(rb).await?;
     content_formatting::exec(rb).await?;
+    seed_placeholder_ru::exec(rb).await?;
     Ok(())
 }
